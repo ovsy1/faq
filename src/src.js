ec17683ce84b2d3e03c5b02ad7ae711b2c47e9a4
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-08-17 19:15:31 +0300

    one more fix...

--- src/src.js
+++ src/src.js
@@ -5,6 +5,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    this.classList.toggle("open");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
@@ -14,9 +15,5 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-
-document.getElementsByClassName('collapsible').onclick = function() {
-  document.getElementsByClassName('faq__li').classList.add('change');
-}
 
 
